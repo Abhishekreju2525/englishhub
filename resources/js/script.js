489bb07a7edb9c164c425d563997579d9ce8004d
Abhishek S R <68371958+Abhishekreju2525@users.noreply.github.com>
2021-06-23 10:24:16 +0530

    Update script.js

--- resources/js/script.js
+++ resources/js/script.js
@@ -93,3 +93,12 @@ $('#' + viewName).show();
     });
 
 });
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
